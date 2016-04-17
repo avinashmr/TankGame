c6c4667e2404b2181ed36ccdf2a0046e3b9bc157
cruiser0002 <cruiser0002@MacBook-Air.local>
2016-04-16 20:51:28 -0700

    added physics

--- SpriteKitTV/GameScene.swift
+++ SpriteKitTV/GameScene.swift
@@ -84,7 +84,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         addChild(grass)
         
         
-        //backgroundColor = SKColor.darkGrayColor()
+//        backgroundColor = SKColor.darkGrayColor()
+        
         // 3
         player.position = CGPoint(x: size.width * 0.1, y: size.height * 0.5)
         player.zRotation = CGFloat(90.0.degreesToRadians)
@@ -101,25 +102,18 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         
-        // 4
         addChild(player)
         
-        let gravityField = SKFieldNode.radialGravityField()
-        gravityField.position = player.position
-        gravityField.strength = 0.1
+        let vortexField = SKFieldNode.vortexField()
+        vortexField.position = CGPoint(x: size.width/2, y: size.height/2)
+        vortexField.strength = 1
         
-        addChild(gravityField)
+        addChild(vortexField)
         
         
         self.physicsWorld.gravity = CGVectorMake(0, 0)
         physicsWorld.contactDelegate = self
-        
-//        runAction(SKAction.repeatActionForever(
-//            SKAction.sequence([
-//                SKAction.runBlock(addMonster),
-//                SKAction.waitForDuration(1.0)
-//                ])
-//            ))
+
         
         runAction(SKAction.repeatActionForever(
             SKAction.sequence([
@@ -127,6 +121,14 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 SKAction.waitForDuration(1.0)
                 ])
             ))
+        
+//        runAction(SKAction.repeatActionForever(
+//            SKAction.sequence([
+//                SKAction.runBlock(addField),
+//                SKAction.waitForDuration(1.0)
+//                ])
+//            ))
+        
         
         let backgroundMusic = SKAudioNode(fileNamed: "background-music-aac.caf")
         backgroundMusic.autoplayLooped = true
@@ -152,24 +154,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         
         let offset = CGPoint(x: sin(player.zRotation), y: -cos(player.zRotation))
-//        print(offset)
-//        print(player.zRotation)
+
         
         let direction = offset.normalized()
         
-        // 7 - Make it shoot far enough to be guaranteed off screen
-        let shootAmount = direction * 10000
+        let shootAmount = direction * size.width
         
-        // 8 - Add the shoot amount to the current position
         let realDest = shootAmount + projectile.position
         
-        // 9 - Create the actions
         let actionMove = SKAction.moveTo(realDest, duration: 2.0)
         
         let actionMoveDone = SKAction.removeFromParent()
         projectile.runAction(SKAction.sequence([actionMove, actionMoveDone]))
         
         addChild(projectile)
+        
+        
+    }
+    
+    func addField() {
+        
         
         
     }
@@ -184,7 +188,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func rotatePlayer(gesture: UIPanGestureRecognizer) {
         let relativeLocation = gesture.translationInView(self.view)
         let relativeVelocity = gesture.velocityInView(self.view)
-        print("\(relativeLocation) : \(relativeVelocity)")
+//        print("\(relativeLocation) : \(relativeVelocity)")
         
         switch gesture.state {
         case .Changed:
